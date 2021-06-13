18d1902fe2a3c650e00fb1072be71da563430c34
IEatSushi <2klaus.4peter@gmail.com>
2021-06-13 12:09:26 +0200

    prevent repetitive key press

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function repaint() {
 	//2D graphics context
 	let ctx = canvas.getContext("2d");
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	
+
 	for (let entry of circles) {
 		let circle = entry[1];
 		circle.display(ctx);
@@ -45,15 +45,24 @@ function handleTouchEnd(event) {
 function handleTouchMove(event) {
 }
 
+let keyDown = {}
+
 function handleKeyDown(event) {
+	if (keyDown[event.keyCode]) {
+		return;
+	}
 	let pressedKey = String.fromCharCode(event.keyCode);
 	let circle = new Circle(pressedKey, Math.random() * screenWidth, Math.random() * screenHeight, 30 * pixelRatio);
 	circles.set(pressedKey, circle);
+
+	keyDown[event.keyCode] = true;
 	repaint();
 }
 
 function handleKeyUp(event) {
+	keyDown[event.keyCode] = false;
 	let pressedKey = String.fromCharCode(event.keyCode);
+
 	circles.delete(pressedKey);
 	repaint();
 }
